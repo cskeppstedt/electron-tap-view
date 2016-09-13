e4c90e90e8532a67512ca5842357dfa749877f02
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2016-09-13 23:48:10 +0200

    Style the Plan based on current status

--- app/components/Plan.js
+++ app/components/Plan.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import styles from './Plan.css'
 
-const getCount = (currentCount, nextEstimatedCount) => (
-  currentCount && nextEstimatedCount
-    ? `${currentCount}/${Math.max(currentCount, nextEstimatedCount)}`
-    : 'Waiting for input'
-)
+const status = ({ plan, nextEstimatedCount, currentCount }) => {
+  if (plan) {
+    return `Done with plan ${plan.start}..${plan.end}`
+  } else if (currentCount && nextEstimatedCount) {
+    return `Collecting ${currentCount}/${Math.max(currentCount, nextEstimatedCount)}...`
+  } else if (currentCount) {
+    return `Collection ${currentCount}...`
+  } else {
+    return 'Waiting for TAP input'
+  }
+}
 
-export default ({ plan, nextEstimatedCount, currentCount }) => (
-  <h3 className={styles.title}>
-    {plan
-      ? `Done [${plan.start}..${plan.end}]`
-      : `Running (${getCount(currentCount, nextEstimatedCount)})`
-    }
-  </h3>
+export default (props) => (
+  <span className={styles.root}>
+    {status(props)}
+  </span>
 )
