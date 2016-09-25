5e5781b0d4d8cb3006620520e5623be37573869a
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2016-09-25 14:51:31 +0200

    Style the assertions

--- app/components/Assert.js
+++ app/components/Assert.js
@@ -18,6 +18,37 @@ export default ({ assert }) => {
       <span className={styles.name}>
         {assert.name}
       </span>
+      {assert.diag && renderDiag(assert.diag)}
     </div>
+  )
+}
+
+function renderDiag ({ actual, expected, operator, at }) {
+  return (
+    <div className={styles.diag}>
+      <div>
+        Expected {expectation(actual, expected, operator)}
+      </div>
+      <div>
+        {assertionLocation(at)}
+      </div>
+    </div>
+  )
+}
+
+function expectation (actual, expected, operator) {
+  return (
+    <span className={styles.expectation}>
+      {actual} {operator} {expected}
+    </span>
+  )
+}
+
+function assertionLocation (at) {
+  // idea: make path clickable and launch in editor
+  return (
+    <span className={styles.location}>
+      {at}
+    </span>
   )
 }
